5a89dbfe634272c6545220989061122d473bae7e
qchempku2017 <664222956@qq.com>
2019-06-14 14:40:17 -0700

    Fri Jun 14 14:40:17 PDT 2019

--- check_vasprun.py
+++ check_vasprun.py
@@ -16,11 +16,22 @@ def _is_successful_vasp(root,files):
     out_path = os.path.join(root,'OUTCAR')
     with open(out_path) as outfile:
         out_string = outfile.read()
-    if 'reached required accuracy' not in ourcar_string:
-        print('Instance {} did not converge.'.format(root))
-        return False
+        if 'reached required accuracy' not in out_string:
+            print('Instance {} did not converge.'.format(root))
+            return False
     return True
 
+fails = []
+n_fails = 0
+n_success = 0
 for root, dirs, files in os.walk('./vasp_run'):
     if _is_successful_vasp(root,files):
         print("Instance {} is successful!".format(root))
+        n_success += 1
+    else:
+       if _is_vasp_input(files) and _have_vasp_output(files):
+           fails.append(root)
+           n_fails += 1
+
+print("Failed structures:\n",fails)
+print("Fail rate {}/{}".format(n_fails,n_fails+n_success))
