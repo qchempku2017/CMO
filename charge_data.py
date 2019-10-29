3eb384b4f48766eb83776445ed832ddf117e98b3
Izzy Xie <664222956@qq.com>
2019-10-28 22:08:19 -0700

    Add files via upload

---
+++ charge_data.py
@@ -0,0 +1,37 @@
+Domains_dict = \
+{
+    'mag':
+    {
+        'Mn':
+        {
+            4:(2.5,3.3),
+            3:(3.7,4.1),
+            2:(4.5,5.0),
+        } #More elements yet to come!
+
+    },
+    'chg':
+    {
+        #Yet to come!
+    }
+}
+
+Fix_Charges_dict = \
+{
+    'F':-1,
+    'Cl':-1,
+    'O':-2,
+    'S':-2,
+    'N':-3,
+    'C':-4,
+    'H':1,
+    'Li':1,
+    'Na':1,
+    'K':1,
+    'Mg':2,
+    'Ca':2,
+    'Sr':2,
+    'Ba':2,
+    'Al':3,
+    'La':3
+}
